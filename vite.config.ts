24b2f8f810274aa35dd1a3463c60d51ea326e706
Adrian <kontakt@antara.pl>
2025-07-30 17:59:09 -0700

    v1.2.6 vite config for node

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import pkg from './package.json'
 
 export default defineConfig({
+  // See: https://vite.dev/config/ssr-options.html
+  ssr: {
+
+    // No dependencies are externalized. However, dependencies explicitly listed 
+    // in ssr.external (using string[] type) can take priority and still be externalized.
+    noExternal: true, 
+
+    // devDependencies are externalized (not included in the bundle)
+    external: Object.keys(pkg.devDependencies), 
+
+    // Node.js built-ins will also be externalized by default.
+    target: 'node'
+  },
   build: {
+    ssr: true,
     lib: {
       entry: {
         main: 'src/main.ts',
@@ -11,7 +26,6 @@ export default defineConfig({
       formats: ['es']
     },
     rollupOptions: {
-      external: ['fs', 'path'],
       output: {
         globals: {}
       }
